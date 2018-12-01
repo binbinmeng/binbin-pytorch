fae48e936077e748bf05c9cb7dbc1c2224696a0e
binbinmeng <15221657504@163.com>
2018-12-01 15:25:07 +0800

    add torchvision, under it includes models and datasets.developper can customized self ops in them

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
-from fp16util import *
+from mixedprecisions.fp16util import *
 
 model_names = sorted(name for name in models.__dict__
                      if name.islower() and not name.startswith("__")
