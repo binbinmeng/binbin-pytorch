c769e858c363e9ba56915d765cc8646a6119ba2c
binbinmeng <15221657504@163.com>
2018-12-03 08:37:45 +0000

    add

--- dl_algorithms/face_detection/mtcnn/image_dataloader/__init__.py
+++ dl_algorithms/face_detection/mtcnn/image_dataloader/__init__.py
@@ -1,3 +1,3 @@
 from .image_database import *
 from .image_loader import *
-from .image_converter import
+from .image_converter import *
--- dl_algorithms/face_detection/mtcnn/train_networks/train_pnet.py
+++ dl_algorithms/face_detection/mtcnn/train_networks/train_pnet.py
@@ -1,22 +1,24 @@
 import argparse
 import sys
+import os
 #from dface.core.imagedb import ImageDB
 #from dface.train_net.train import train_pnet
 #import dface.config as config
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from image_dataloader import image_database
 import os
-
+import yaml
+f = open('../gen_train_data/config.yaml', encoding='utf-8')
+configure = yaml.load(f)
 
 
 def train_net(annotation_file, model_store_path,
                 end_epoch=16, frequent=200, lr=0.01, batch_size=128, use_cuda=False):
-
-    imagedb = ImageDB(annotation_file)
+    imagedb = image_database.ImageDataBase(annotation_file)
     gt_imdb = imagedb.load_imdb()
     gt_imdb = imagedb.append_flipped_images(gt_imdb)
 
-    train_pnet(model_store_path=model_store_path, end_epoch=end_epoch, imdb=gt_imdb, batch_size=batch_size, frequent=frequent, base_lr=lr, use_cuda=use_cuda)
+    #train_pnet(model_store_path=model_store_path, end_epoch=end_epoch, imdb=gt_imdb, batch_size=batch_size, frequent=frequent, base_lr=lr, use_cuda=use_cuda)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train PNet',
@@ -24,19 +26,19 @@ def parse_args():
 
 
     parser.add_argument('--anno_file', dest='annotation_file',
-                        default=os.path.join(config.ANNO_STORE_DIR,config.PNET_TRAIN_IMGLIST_FILENAME), help='training data annotation file', type=str)
+                        default=os.path.join(configure["ANNO_STORE_DIR"],configure["PNET_TRAIN_IMGLIST_FILENAME"]), help='training data annotation file', type=str)
     parser.add_argument('--model_path', dest='model_store_path', help='training model store directory',
-                        default=config.MODEL_STORE_DIR, type=str)
+                        default=configure["MODEL_STORE_DIR"], type=str)
     parser.add_argument('--end_epoch', dest='end_epoch', help='end epoch of training',
-                        default=config.END_EPOCH, type=int)
+                        default=configure["END_EPOCH"], type=int)
     parser.add_argument('--frequent', dest='frequent', help='frequency of logging',
                         default=200, type=int)
     parser.add_argument('--lr', dest='lr', help='learning rate',
-                        default=config.TRAIN_LR, type=float)
+                        default=configure["TRAIN_LR"], type=float)
     parser.add_argument('--batch_size', dest='batch_size', help='train batch size',
-                        default=config.TRAIN_BATCH_SIZE, type=int)
+                        default=configure["TRAIN_BATCH_SIZE"], type=int)
     parser.add_argument('--gpu', dest='use_cuda', help='train with gpu',
-                        default=config.USE_CUDA, type=bool)
+                        default=configure["USE_CUDA"], type=bool)
     parser.add_argument('--prefix_path', dest='', help='training data annotation images prefix root path', type=str)
 
     args = parser.parse_args()
