fac67c69b3bba2e0c4dee1e96f2461347b3a1970
binbinmeng <15221657504@163.com>
2018-12-03 16:04:20 +0800

    add

---
+++ dl_algorithms/face_detection/mtcnn/image_dataloader/__init__.py
@@ -0,0 +1,3 @@
+from .image_database import *
+from .image_loader import *
+from .image_converter import
--- dl_algorithms/face_detection/mtcnn/train_networks/train_pnet.py
+++ dl_algorithms/face_detection/mtcnn/train_networks/train_pnet.py
@@ -3,6 +3,8 @@ import sys
 #from dface.core.imagedb import ImageDB
 #from dface.train_net.train import train_pnet
 #import dface.config as config
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from image_dataloader import image_database
 import os
 
 
